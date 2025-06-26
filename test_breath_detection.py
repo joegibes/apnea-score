b6dd2d4a20f4e6701debc63716ff93346548dec5
Joe Gibes <joegibes@gmail.com>
2025-06-25 23:12:16 -0400

    cursor cookin with the session_processing and stuff

---
+++ test_breath_detection.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from src.breath_detection import detect_breaths_from_flow
+
+# Load merged session data
+merged_path = 'data/2025/20250617_023551_merged.parquet'
+df = pd.read_parquet(merged_path)
+
+# Run breath detection on Flow.40ms
+sampling_freq_hz = 25  # Known from your data
+breaths = detect_breaths_from_flow(df['Flow.40ms'], sampling_freq_hz)
+
+print('Detected breaths:', len(breaths))
+print(breaths.info())
+print(breaths.head()) 
